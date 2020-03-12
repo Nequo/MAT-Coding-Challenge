24e3335e58d14bb057fbd926bba79ecc7e5d28e2
Nequo <nequo@github.com>
2020-03-12 14:51:31 +0000

    Refactor so order of operations makes more sense

--- src/backend.py
+++ src/backend.py
@@ -2,7 +2,17 @@ import paho.mqtt.client as mqtt
 import time
 
 broker_address = "127.0.0.1"
+
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe("carCoordinates")
+
+def on_message(client, userdata, message):
+    print("Received: ", str(message.payload.decode("utf-8")))
+
 client = mqtt.Client("Positions")
+client.on_message = on_message
+client.on_connect = on_connect
 
 try:
     client.connect(broker_address)
@@ -10,12 +20,6 @@ except:
     print("Could not connect to broker, are you sure you ran docker-compose up?")
     exit(1)
 
-client.subscribe("carCoordinates")
-
-def on_message(client, userdata, message):
-    print("Received: ", str(message.payload.decode("utf-8")))
-
-client.on_message = on_message
 client.loop_start()
 time.sleep(4)
 client.loop_stop()
